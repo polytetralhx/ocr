8f8085ca42019af5fc569dfdbcdf9bb24f154ac1
polytetralhx <87745260+polytetralhx@users.noreply.github.com>
2023-06-21 10:48:59 +0800

    update dashboard to debug code

--- dashboard.py
+++ dashboard.py
@@ -22,6 +22,7 @@ def main():
             if uploaded_img is not None:
                 image = Image.open(uploaded_img)
                 st.header("Image Uploaded for OCR")
+                st.write(type(image))
                 st.image(image)
                 
                 image2 = OCRImg(image)
@@ -43,6 +44,7 @@ def main():
         
         if picture is not None:
             st.header("Image Taken for OCR")
+            st.write(type(picture))
             st.image(picture)
             
             pic2 = OCRImg(picture)
