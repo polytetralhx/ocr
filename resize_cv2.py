a9c36867adb9af0d82d2b96e983ed9ff7b0daa8c
polytetralhx <87745260+polytetralhx@users.noreply.github.com>
2023-06-28 14:15:54 +0800

    added new test cases, and padding for images

---
+++ resize_cv2.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+
+from typing import Tuple
+
+def resize_with_pad(image: np.array, 
+                    new_shape: Tuple[int, int], 
+                    padding_color: Tuple[int] = (0, 0, 0)) -> np.array: # made black pad by default
+    """Maintains aspect ratio and resizes with padding.
+    Params:
+        image: Image to be resized.
+        new_shape: Expected (width, height) of new image.
+        padding_color: Tuple in BGR of padding color
+    Returns:
+        image: Resized image with padding
+    """
+    original_shape = (image.shape[1], image.shape[0])
+    ratio = float(max(new_shape))/max(original_shape)
+    new_size = tuple([int(x*ratio) for x in original_shape])
+    #image = cv2.resize(image, new_size)
+    delta_w = new_shape[0] - new_size[0]
+    delta_h = new_shape[1] - new_size[1]
+    top, bottom = delta_h//2, delta_h-(delta_h//2)
+    left, right = delta_w//2, delta_w-(delta_w//2)
+    image = cv2.copyMakeBorder(image, top, bottom, left, right, cv2.BORDER_CONSTANT, value=padding_color)
+    
+    return image
