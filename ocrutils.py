4d68ddd879a4db777fdb0b165dc23c871c4aa606
polytetralhx <87745260+polytetralhx@users.noreply.github.com>
2023-06-30 14:01:36 +0800

    edit helper functions

--- ocrutils.py
+++ ocrutils.py
@@ -5,12 +5,6 @@ from sklearn.metrics import euclidean_distances
 
 DOC_WIDTH, DOC_HEIGHT = 1098, 648
 
-def pil_as_array(pil_img):
-    rgb_img = pil_img.convert("RGB")
-    open_cv_image = np.array(rgb_img) 
-    open_cv_image = open_cv_image[:, :, ::-1].copy() 
-    return open_cv_image
-
 def gamma_correction(src, gamma):
     invGamma = 1 / gamma
 
@@ -18,6 +12,12 @@ def gamma_correction(src, gamma):
     table = np.array(table, np.uint8)
 
     return cv2.LUT(src, table)
+
+def pil_as_array(pil_img):
+    rgb_img = pil_img.convert("RGB")
+    open_cv_image = np.array(rgb_img) 
+    open_cv_image = open_cv_image[:, :, ::-1].copy() 
+    return open_cv_image
 
 def get_padding(img):
     '''add padding equivalent to 10% of the document size and return image'''
@@ -39,7 +39,7 @@ def get_edges(img):
         output the same image reduced to its edges.'''
     # convert the image to grayscale, blur it, and find edges
     # in the image
-    gray = cv2.cvtColor(gammaCorrection(img, 1.05), cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(gamma_correction(img, 1.05), cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (5, 5), 0)
     edged = cv2.Canny(gray, 70, 150)
     
@@ -67,32 +67,34 @@ def select_coords(screenCnt):
     screenCnt = screenCnt.reshape(len(screenCnt), 2)
     screenCnt[:, 0] -= int(0.1 * DOC_WIDTH)
     screenCnt[:, 1] -= int(0.1 * DOC_HEIGHT)
-    close_pairs = np.array(np.where(~((euclidean_distances(screenCnt, screenCnt) > 50) | (euclidean_distances(screenCnt, screenCnt) == 0)))).T
+    close_pairs = np.array(np.where(~((euclidean_distances(screenCnt, screenCnt) > 10) | (euclidean_distances(screenCnt, screenCnt) == 0)))).T
 
-    pair_cleaned = []
+    if len(close_pairs):
+        pair_cleaned = []
 
-    for p1, p2 in close_pairs:
-        a = False
-        for x1, x2 in pair_cleaned:
-            if p1 == x2:
-                a = not a
-                break
-        if not a:
-            pair_cleaned.append((p1, p2))
+        for p1, p2 in close_pairs:
+            a = False
+            for x1, x2 in pair_cleaned:
+                if p1 == x2:
+                    a = not a
+                    break
+            if not a:
+                pair_cleaned.append((p1, p2))
 
-    remove_points = np.array(pair_cleaned)[:, 1]
+        remove_points = np.array(pair_cleaned)[:, 1]
 
-    mask = np.ones(len(screenCnt), dtype=bool) 
-    mask[remove_points] = False
-    result = screenCnt[mask]
+        mask = np.ones(len(screenCnt), dtype=bool) 
+        mask[remove_points] = False
+        screenCnt = screenCnt[mask]
 
-    #sorted_points = np.array(sorted(result, key=lambda x: x[0]))
-    left, right, top, bottom = min(result[:, 0]), max(result[:, 0]), min(result[:, 1]), max(result[:, 1])
+    #sorted_points = np.array(sorted(screenCnt, key=lambda x: x[0]))
+    left, right, top, bottom = min(screenCnt[:, 0]), max(screenCnt[:, 0]), min(screenCnt[:, 1]), max(screenCnt[:, 1])
+    #print(left, right, top, bottom)
 
-    topleft = min(result, key=lambda x: euclidean_distances([x], [(left, top)]))
-    topright = min(result, key=lambda x: euclidean_distances([x], [(right, top)]))
-    bottomleft = min(result, key=lambda x: euclidean_distances([x], [(left, bottom)]))
-    bottomright = min(result, key=lambda x: euclidean_distances([x], [(right, bottom)]))
+    topleft = min(screenCnt, key=lambda x: euclidean_distances([x], [(left, top)]))
+    topright = min(screenCnt, key=lambda x: euclidean_distances([x], [(right, top)]))
+    bottomleft = min(screenCnt, key=lambda x: euclidean_distances([x], [(left, bottom)]))
+    bottomright = min(screenCnt, key=lambda x: euclidean_distances([x], [(right, bottom)]))
     
     return topleft, topright, bottomleft, bottomright
 
