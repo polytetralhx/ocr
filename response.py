cfa679455f28f29457991922e9b0d613adf49a58
polytetralhx <87745260+polytetralhx@users.noreply.github.com>
2023-07-13 16:18:46 +0800

    organizing for docker container

---
+++ response.py
@@ -0,0 +1,14 @@
+import os
+import streamlit as st
+import numpy as np
+from ocrimg import OCRImg
+from ocrutils import transform_image
+from PIL import Image
+import cv2
+import imutils
+
+def main():
+    print("hello world, testing")
+    
+if __name__ == '__main__':
+    main()
